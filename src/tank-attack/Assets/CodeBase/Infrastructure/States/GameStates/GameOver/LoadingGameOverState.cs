70c5c5f1a93513e444225814a911bce391ffc5a4
Gevorg <gulkanyangk@mail.ru>
2025-03-09 21:24:05 +0300

    fix

--- src/tank-attack/Assets/CodeBase/Infrastructure/States/GameStates/GameOver/LoadingGameOverState.cs
+++ src/tank-attack/Assets/CodeBase/Infrastructure/States/GameStates/GameOver/LoadingGameOverState.cs
@@ -10,19 +10,13 @@ namespace CodeBase.Infrastructure.States.GameStates.GameOver
     public class LoadingGameOverState : SimpleState
     {
         private readonly ISceneLoader _sceneLoader;
-        private readonly IGameStateMachine _stateMachine;
-        private readonly IStaticDataService _staticDataService;
         private readonly LoadingCurtain _loadingCurtain;
 
         public LoadingGameOverState(
             ISceneLoader sceneLoader,
-            IGameStateMachine stateMachine,
-            IStaticDataService staticDataService,
             LoadingCurtain loadingCurtain)
         {
             _sceneLoader = sceneLoader;
-            _stateMachine = stateMachine;
-            _staticDataService = staticDataService;
             _loadingCurtain = loadingCurtain;
         }
         
