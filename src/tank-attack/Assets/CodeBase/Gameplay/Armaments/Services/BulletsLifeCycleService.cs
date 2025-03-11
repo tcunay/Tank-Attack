500944b36294b1dee746dbd9a0cd747b7e942e4e
Gevorg <gulkanyangk@mail.ru>
2025-03-11 17:46:05 +0300

    fix

--- src/tank-attack/Assets/CodeBase/Gameplay/Armaments/Services/BulletsLifeCycleService.cs
+++ src/tank-attack/Assets/CodeBase/Gameplay/Armaments/Services/BulletsLifeCycleService.cs
@@ -11,15 +11,14 @@ namespace CodeBase.Gameplay.Armaments.Services
     {
         private readonly IProjectileFactory _factory;
         private readonly ICoroutineRunner _coroutineRunner;
-
-        public event Action CountChanged;
-
+        
         public BulletsLifeCycleService(IProjectileFactory factory, ICoroutineRunner coroutineRunner)
         {
             _factory = factory;
             _coroutineRunner = coroutineRunner;
         }
         
+        public event Action CountChanged;
         public int LiveBulletsCount { get; private set; }
         
         public void Initialize()
